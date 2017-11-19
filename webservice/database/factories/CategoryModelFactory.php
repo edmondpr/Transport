7019b85d743dbc0e313b3d3d6186bdad36621e21
Edmond Pruteanu <edmondpr@gmail.com>
2017-11-19 13:11:53 +0200

    Adaugat template aplicatie Vue.js

--- webservice/database/factories/CategoryModelFactory.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$factory->define(App\Category::class, function (Faker\Generator $faker) {
-    return [
-        'name' => $faker->word,
-    ];
-});
--- webservice/database/factories/ProductModelFactory.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$factory->define(App\Product::class, function (Faker\Generator $faker) {
-    return [
-        'name' => $faker->sentence(6, true),
-    ];
-});
